3a6fa0c093ca71fd4e76fea515a29c2a225fcd80
Tiago <t.branquinho.slb@gmail.com>
2022-10-21 10:38:20 +0100

    Comented some code

--- code/include/application_layer.h
+++ code/include/application_layer.h
@@ -4,6 +4,15 @@
 #ifndef _APPLICATION_LAYER_H_
 #define _APPLICATION_LAYER_H_
 
+#define CTRL_DATA       0x01
+#define CTRL_START      0x02
+#define CTRL_END        0x03
+
+struct applicationLayer {
+    int fileDescriptor; /*Descritor correspondente à porta série*/
+    int status; /*TRANSMITTER | RECEIVER*/
+};
+
 // Application layer main function.
 // Arguments:
 //   serialPort: Serial port name (e.g., /dev/ttyS0).
@@ -15,4 +24,9 @@
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename);
 
+// build data packet / control packet
+
 #endif // _APPLICATION_LAYER_H_
+
+
+// llopen() llread() llwrite() llclose()
