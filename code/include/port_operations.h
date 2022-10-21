bb7988e3f22a88d4b151ebe554f568176f69e30c
Francisco Cerqueira <up201905337@fe.up.pt>
2022-10-21 11:50:12 +0100

    Port operations file update

---
+++ code/include/port_operations.h
@@ -0,0 +1,25 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <time.h>
+
+// Baudrate settings are defined in <asm/termbits.h>, which is
+// included by <termios.h>
+#define BAUDRATE B38400
+#define _POSIX_SOURCE 1 // POSIX compliant source
+
+#define FALSE 0
+#define TRUE 1
+
+struct termios oldtio;
+struct termios newtio;
+
+int set_up_port(const char *serialPortName);
+int set_up_port(int *fd);
--- code/src/port_operations.c
+++ code/src/port_operations.c
@@ -1,25 +1,4 @@
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <termios.h>
-#include <unistd.h>
-#include <signal.h>
-#include <stdio.h>
-#include <time.h>
-
-// Baudrate settings are defined in <asm/termbits.h>, which is
-// included by <termios.h>
-#define BAUDRATE B38400
-#define _POSIX_SOURCE 1 // POSIX compliant source
-
-#define FALSE 0
-#define TRUE 1
-
-struct termios oldtio;
-struct termios newtio;
+#include "port_operations.h"
 
 int set_up_port(const char *serialPortName){
 
