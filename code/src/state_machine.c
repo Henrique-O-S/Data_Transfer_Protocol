1e5eae88d8d7f9074b7846d54b8e5a645be3a655
Henrique-O-S <henri11silvafcc@gmail.com>
2022-11-02 17:39:37 +0000

    Updated report

--- code/src/state_machine.c
+++ code/src/state_machine.c
@@ -1,12 +1,6 @@
 #include "state_machine.h"
 
 
-/**
- * Function to check if a byte is contained in the state machine's controlBytes field
- * @param byte Byte to be checked
- * @param sm State machine for which to check
- * @return Index of the array where the byte was found; negative if byte is not a member
- */
 int isControlByte(unsigned char byte, state_machine_st* sm) {
   for (int i = 0; i < sm->controlBytesLength; i++) {
     if (sm->controlBytes[i] == byte)
@@ -17,23 +11,11 @@ int isControlByte(unsigned char byte, state_machine_st* sm) {
 }
 
 
-/**
- * Function to update the state of the state machine
- * @param sm State machine to be updated
- * @param st State to be assigned to the state machine
- */
 void change_state(state_machine_st* sm, state_st st) {
     sm->state = st;
 }
 
 
-/**
- * Function to create a state machine, with the given attributes
- * @param controlBytes Possible bytes that are expected in the frame to be read by the state machine
- * @param controlBytesLength Number of possible bytes that are expected in the frame to be read by the state machine
- * @param addressByte Address from which the frame to be read by the state machine is expected
- * @return Pointer to the new state machine "object" (struct)
- */
 state_machine_st* create_state_machine(unsigned char* controlBytes, int controlBytesLength, unsigned char addressByte) {
     state_machine_st* sm = malloc(sizeof(state_machine_st));
     change_state(sm, START);
@@ -45,13 +27,6 @@ state_machine_st* create_state_machine(unsigned char* controlBytes, int controlB
 }
 
 
-/**
- * Function to update the state machine according to the bytes read
- * @param sm State machine to be updated
- * @param byte Last byte to have been read, of the current frame
- * @param frame Address where the frame that's being read is being stored
- * @param mode Type of frame that's being read (Supervision or Information)
- */
 void event_handler(state_machine_st* sm, unsigned char byte, unsigned char* frame, int mode) {
 
     static int i = 0;
@@ -199,10 +174,6 @@ void event_handler(state_machine_st* sm, unsigned char byte, unsigned char* fram
 }
 
 
-/**
- * Function to free the memory allocated to a state machine object
- * @param sm State machine to be destroyed
- */
 void destroy_st(state_machine_st* sm) {
   free(sm);
 }
